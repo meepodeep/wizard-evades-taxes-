3d46dbbfd71c703d77208d0bbda4d895f7193baf
meepodeep <nbltmepodep@gmail.com>
2024-07-23 20:05:02 -0400

    worked on crowd behavior

--- Scripts/States/EnemyFollow.gd
+++ Scripts/States/EnemyFollow.gd
@@ -1,28 +1,52 @@
 extends State
 class_name EnemyFollow
-@onready var raycasts = $"../../Raycasts"
+@onready var raycast = $"../../Raycasts"
 @export var enemy : CharacterBody2D
 @export var move_speed := 40.0
 var player : CharacterBody2D
-
+var directionDodge = Vector2(0,0)
+var raycasts = []
 func Enter():
 	player = get_tree().get_first_node_in_group("Player")
 
 func Physics_Update(delta: float):
-	for child in raycasts.get_children():
-		if child.is_colliding():
-			match child.name.to_lower():
-				"raycastnorth":
-					print("raycastnorth")
-				"raycastsouth":
-					print("raycastsouth")
-				"raycasteast":
-					print("raycasteast")
-				"raycastwest":
-					print("raycastwest")
+	raycasts = raycast.get_children()
+	print(directionDodge)
+
+	for child in raycasts:
+		if false in raycasts:
+			directionDodge = Vector2(0,0)
+		match child.name.to_lower():
+			"raycastnorth":
+				if child.is_colliding():
+					directionDodge += Vector2(0,1)
+			"raycastsouth":
+				if child.is_colliding():
+					directionDodge += Vector2(0,-1)
+			"raycasteast":
+				if child.is_colliding():
+						directionDodge += Vector2(-1,0)
+			"raycastwest":
+				if child.is_colliding():
+						directionDodge += Vector2(1,0)
+			"raycastsoutheast":
+				if child.is_colliding():
+						directionDodge += Vector2(-1,-1)
+			"raycastnortheast":
+				if child.is_colliding():
+						directionDodge += Vector2(-1,1)
+			"raycastnorthwest":
+				if child.is_colliding():
+						directionDodge += Vector2(1,1)
+			"raycastsouthwest":
+				if child.is_colliding():
+					directionDodge += Vector2(1,-1)
 			
-	var direction = player.global_position - enemy.global_position
-	if direction.length() > 50:
+
+					
+				
+	var direction = player.global_position - enemy.global_position + directionDodge
+	if direction.length() > 0:
 		enemy.velocity = direction.normalized() * move_speed
 	else:
 		enemy.velocity = Vector2()
